dfba54942e1fa7e5b8f5dc4d3b2317f26f57054c
mc0re <mnikitin@hotmail.com>
2018-11-24 14:41:32 +0100

    Added settings.

--- CityInfo/CityInfo/Controllers/PoiController.cs
+++ CityInfo/CityInfo/Controllers/PoiController.cs
@@ -1,6 +1,9 @@
 ﻿using CityInfo.Models;
+using CityInfo.Services;
 using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Linq;
 
 namespace CityInfo.Controllers
@@ -8,17 +11,42 @@ namespace CityInfo.Controllers
     [Route("api/cities")]
     public class PoiController : Controller
     {
+        #region Fields
+
+        private ILogger<PoiController> mLogger;
+
+        private ISenderService mSender;
+
         private static CityDto GetCityOrNothing(int cityId)
         {
             return CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
         }
 
+        #endregion
+
+
+        #region Init and clean-up
+
+        public PoiController(ILogger<PoiController> logger, ISenderService sender)
+        {
+            mLogger = logger;
+            mSender = sender;
+        }
+
+        #endregion
+
+
+        #region HTTP requests
 
         [HttpGet("{cityId}/poi")]
         public IActionResult GetPois(int cityId)
         {
             var city = GetCityOrNothing(cityId);
-            if (city == null) return NotFound();
+            if (city == null)
+            {
+                mLogger.LogInformation($"City {cityId} not found.");
+                return NotFound();
+            }
 
             return Ok(city.Poi);
         }
@@ -75,9 +103,9 @@ namespace CityInfo.Controllers
         [HttpPut("{cityId}/poi/{id}")]
         public IActionResult UpdatePoi(int cityId, int id, [FromBody] PoiUpdateDto poi)
         {
-            var validator = new PoiValidator();
-            var validation = validator.Validate(poi);
-            validation.AddToModelState(ModelState, null);
+            //var validator = new PoiValidator();
+            //var validation = validator.Validate(poi);
+            //validation.AddToModelState(ModelState, null);
 
             if (!ModelState.IsValid)
             {
@@ -95,5 +123,52 @@ namespace CityInfo.Controllers
 
             return NoContent();
         }
+
+
+        [HttpPatch("{cityId}/poi/{id}")]
+        public IActionResult PatchPoi(int cityId, int id, [FromBody] JsonPatchDocument<PoiUpdateDto> patch)
+        {
+            if (patch == null)
+            {
+                return BadRequest();
+            }
+
+            var city = GetCityOrNothing(cityId);
+            if (city == null) return NotFound(new { CityId = cityId });
+
+            var existingPoi = city.Poi.FirstOrDefault(p => p.Id == id);
+            if (existingPoi == null) return NotFound(new { PoiId = id });
+
+            var patched = new PoiUpdateDto { Name = existingPoi.Name, Description = existingPoi.Description };
+            patch.ApplyTo(patched, ModelState);
+            TryValidateModel(patched);
+
+            if (! ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            existingPoi.Name = patched.Name;
+            existingPoi.Description = patched.Description;
+
+            return NoContent();
+        }
+
+
+        [HttpDelete("{cityId}/poi/{id}")]
+        public IActionResult DeletePoi(int cityId, int id)
+        {
+            var city = GetCityOrNothing(cityId);
+            if (city == null) return NotFound(new { CityId = cityId });
+
+            var existingPoi = city.Poi.FirstOrDefault(p => p.Id == id);
+            if (existingPoi == null) return NotFound(new { PoiId = id });
+
+            city.Poi.Remove(existingPoi);
+            mSender.Send("POI deleted", $"POI {existingPoi.Id} '{existingPoi.Name}' was deleted.");
+            return NoContent();
+        }
+
+        #endregion
     }
 }
--- CityInfo/CityInfo/Startup.cs
+++ CityInfo/CityInfo/Startup.cs
@@ -1,14 +1,28 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using CityInfo.Models;
+using CityInfo.Services;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Serialization;
-
+using NLog.Extensions.Logging;
 
 namespace CityInfo
 {
     public class Startup
     {
+        public static IConfiguration Configuration;
+
+
+        public Startup(IConfiguration conf)
+        {
+            Configuration = conf;
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -21,12 +35,21 @@ namespace CityInfo
 
                 //    reslv.NamingStrategy = null;
                 //})
+                .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<PoiValidator>())
             ;
+            services.AddTransient<ISenderService, LocalMailService>();
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory logger)
         {
+            logger.AddConsole();
+            logger.AddNLog();
+
+            // Debug is registered by default
+            //logger.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
