6575ec81e3d59731564940276262e0823c0e620f
mc0re <mnikitin@hotmail.com>
2018-12-19 23:30:57 +0100

    Version API, v1.

---
+++ VersionedApiLab/VersionedApiLab/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VersionedApiLab.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Version 1.0", "value-1", "value-2", "value-3" };
+        }
+    }
+
+
+    [ApiVersion("1.1")]
+    [Route("api/values")]
+    [ApiController]
+    public class ValuesV11Controller : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Version 1.1", "value-1", "value-2", "value-3" };
+        }
+    }
+}
---
+++ VersionedApiLab/VersionedApiLab/Startup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace VersionedApiLab
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // This setting or CompatibilityVersion.Version_2_1 are needed
+            // to get versioning to work at the moment
+            // https://github.com/Microsoft/aspnet-api-versioning/issues/363
+            services.AddMvc(opt => opt.EnableEndpointRouting = false)
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddApiVersioning(cfg =>
+            {
+                cfg.DefaultApiVersion = new ApiVersion(1, 1);
+                cfg.AssumeDefaultVersionWhenUnspecified = true;
+                cfg.RegisterMiddleware = true;
+                cfg.ReportApiVersions = true;
+            });
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseApiVersioning();
+            app.UseMvc();
+        }
+    }
+}
